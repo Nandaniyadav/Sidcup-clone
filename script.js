23638bb2d1250bd3f3b140232d13fcb480884b27
Nandani Yadav <77967134+Nandaniyadav@users.noreply.github.com>
2023-07-02 21:22:47 +0530

    update code

--- script.js
+++ script.js
@@ -1,10 +1,34 @@
+var crsr = document.querySelector("#cursor")
+document.addEventListener("mousemove", function(dets){
+    crsr.style.left = dets.x + "px"
+    crsr.style.top = dets.y + "px"
+})
+
+
+
+// use gsap
 gsap.to("#nav", {
   backgroundColor: "#000",
-  height: "130px",
+  height: "110px",
   duration: 0.5,
   scrollTrigger: {
     trigger: "#nav",
     scroller: "body",
-    markers: true,
+    // markers: true,
+    start:"top -10%",
+    end:"top -11%",
+    scrub:1,
   },
 });
+
+gsap.to("#main",{
+    backgroundColor:"#000",
+    scrollTrigger:{
+        trigger:"#main",
+        scroller:"body",
+        markers:true,
+        start:"top -25%",
+        end:"top -70%",
+        scrub:2
+    }
+})
