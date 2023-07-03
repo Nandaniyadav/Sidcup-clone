ad74ace18d6019fc8e7de4aa2ff13383010fe1ac
Nandani Yadav <77967134+Nandaniyadav@users.noreply.github.com>
2023-07-03 08:53:27 +0530

    update code

--- script.js
+++ script.js
@@ -1,8 +1,15 @@
+//for cursor
 var crsr = document.querySelector("#cursor")
+var blur = document.querySelector("#cursor-blur")
+
 document.addEventListener("mousemove", function(dets){
     crsr.style.left = dets.x + "px"
     crsr.style.top = dets.y + "px"
+    blur.style.left = dets.x - 250 + "px"
+    blur.style.top =dets.y - 250 + "px"
 })
+
+
 
 
 
