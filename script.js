76249ace56da7be659f7c85638fd5804e7333b9c
Nandani Yadav <77967134+Nandaniyadav@users.noreply.github.com>
2023-07-05 13:51:58 +0530

    update code

--- script.js
+++ script.js
@@ -1,18 +1,16 @@
 //for cursor
-var crsr = document.querySelector("#cursor")
-var blur = document.querySelector("#cursor-blur")
+var crsr = document.querySelector("#cursor");
+var blur = document.querySelector("#cursor-blur");
 
-document.addEventListener("mousemove", function(dets){
-    crsr.style.left = dets.x + "px"
-    crsr.style.top = dets.y + "px"
-    blur.style.left = dets.x - 250 + "px"
-    blur.style.top =dets.y - 250 + "px"
-})
+//mouse move event
+document.addEventListener("mousemove", function (dets) {
+  crsr.style.left = dets.x+30 + "px";
+  crsr.style.top = dets.y + "px";
+  blur.style.left = dets.x - 250 + "px";
+  blur.style.top = dets.y - 250 + "px";
+});
 
-
-
-
-
+//Acroll Trigger
 // use gsap
 gsap.to("#nav", {
   backgroundColor: "#000",
@@ -22,20 +20,36 @@ gsap.to("#nav", {
     trigger: "#nav",
     scroller: "body",
     // markers: true,
-    start:"top -10%",
-    end:"top -11%",
-    scrub:1,
+    start: "top -10%",
+    end: "top -11%",
+    scrub: 1,
   },
 });
 
-gsap.to("#main",{
-    backgroundColor:"#000",
-    scrollTrigger:{
-        trigger:"#main",
-        scroller:"body",
-        markers:true,
-        start:"top -25%",
-        end:"top -70%",
-        scrub:2
-    }
+gsap.to("#main", {
+  backgroundColor: "#000",
+  scrollTrigger: {
+    trigger: "#main",
+    scroller: "body",
+    markers: true,
+    start: "top -25%",
+    end: "top -70%",
+    scrub: 2,
+  },
+});
+
+
+var h4all = document.querySelectorAll("#nav h4")
+h4all.forEach(function(elem){
+  elem.addEventListener("mouseenter",function(){
+    crsr.style.scale = 3.5
+    crsr.style.border="1px solid #fff"
+    crsr.style.backgroundColor="transparent"
+  })
+  elem.addEventListener("mouseleave",function(){
+    crsr.style.scale = 1
+    crsr.style.border="0px solid #95C11E"
+    crsr.style.backgroundColor="#95C11E"
+  })
+  
 })
